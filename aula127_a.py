6fb34af2e6b7ea20764b1f1f13d6c2e55318e02c
Nicole de Assis <nicassissiqueira@gmail.com>
2024-08-31 16:05:46 -0300

    Property

--- aula127_a.py
+++ aula127_a.py
@@ -13,9 +13,12 @@ p1 = Pessoa('João',33)
 p2 = Pessoa('Helena',21)
 p3 = Pessoa('Joana',11)
 
-bd = [vars(p1 )]
+bd = [vars(p1), p2.__dict__, vars(p3)]
 
-bd = [p1,p2,p3]
 
-with open(CAMINHO_ARQUIVO,'w') as arquivo:
-    json.dump(bd,arquivo, ensure_ascii= False, indent = 2)
+def fazer_dump():
+    with open(CAMINHO_ARQUIVO,'w') as arquivo:
+        print('Fazendo Dump')
+        json.dump(bd,arquivo, ensure_ascii= False, indent = 2)
+        
+print("ISSO", __name__)
