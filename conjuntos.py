7f7e582d60268e222cc7805636af0b6b14d03eca
Nicole de Assis <nicassissiqueira@gmail.com>
2024-09-15 23:11:42 -0300

    Conjuntos

---
+++ conjuntos.py
@@ -0,0 +1,9 @@
+conjunto_a= {1,2,3}
+conjunto_b = {2,3, 4}
+
+conjunto_a.union(conjunto_b)
+conjunto_a.intersection(conjunto_b)
+conjunto_a.difference(conjunto_b)
+conjunto_a.symmetric_difference(conjunto_b)
+conjunto_a.issuperset(conjunto_b) #Confirma se pertence
+conjunto_a.isdisjoint(conjunto_b) #Conjunto disjunto
